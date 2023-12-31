e66c8d53d20d809fe3a6f873c3e12ec22f1bd4c4
safiqq <szarifin20041@gmail.com>
2023-12-31 22:00:54 +0700

    chore: fix typo and change env variables name

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -22,10 +22,10 @@ class PlaylistsService {
       text: `SELECT songs.id, 
       songs.title, 
       songs.performer 
-        FROM playlist_songs
+        FROM playlistsongs
         JOIN songs
-        ON playlist_songs.song_id = songs.id 
-        WHERE playlist_songs.playlist_id = $1`,
+        ON playlistsongs.song_id = songs.id 
+        WHERE playlistsongs.playlist_id = $1`,
       values: [playlistId],
     };
 
